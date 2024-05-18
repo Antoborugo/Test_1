0fa16c9049f5f5dce32dd173ad9bfc8db016352b
Antoborugo <lingfei.xing@gmail.com>
2024-05-19 02:53:00 +0700

    Commit

--- app/src/main/java/com/example/test_1/MainActivity2.kt
+++ app/src/main/java/com/example/test_1/MainActivity2.kt
@@ -4,15 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val secondActbutton = findViewById<Button>(R.id.next1)
+        val secondActbutton = findViewById<Button>(R.id.buttonprofpic)
         secondActbutton.setOnClickListener {
+            val Intent = Intent(this,MainActivity5::class.java)
+            startActivity(Intent)
+        }
+        val imageButton = findViewById<ImageButton>(R.id.imageButton)
+        imageButton.setOnClickListener {
             val Intent = Intent(this,MainActivity3::class.java)
+            startActivity(Intent)
+        }
+        val imageButton2 = findViewById<ImageButton>(R.id.imageButton2)
+        imageButton2.setOnClickListener {
+            val Intent = Intent(this,MainActivity4::class.java)
             startActivity(Intent)
         }
     }
--- app/src/main/java/com/example/test_1/MainActivity3.kt
+++ app/src/main/java/com/example/test_1/MainActivity3.kt
@@ -4,14 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
-        val secondActbutton = findViewById<Button>(R.id.next2)
+        val secondActbutton = findViewById<Button>(R.id.buttonprofpic2)
         secondActbutton.setOnClickListener {
+            val Intent = Intent(this,MainActivity5::class.java)
+            startActivity(Intent)
+        }
+        val homegreybutton = findViewById<ImageButton>(R.id.homegreybutton)
+        homegreybutton.setOnClickListener {
+            val Intent = Intent(this,MainActivity2::class.java)
+            startActivity(Intent)
+        }
+        val graycontrolbutton = findViewById<ImageButton>(R.id.graycontrolbutton)
+        graycontrolbutton.setOnClickListener {
             val Intent = Intent(this,MainActivity4::class.java)
             startActivity(Intent)
         }
